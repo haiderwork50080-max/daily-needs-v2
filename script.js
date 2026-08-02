579e12979c131dcd02b6ffa2cc3db547165395a6
haiderwork50080-max <haiderwork50080@gmail.com>
2026-08-02 08:16:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-ghf
+function searchProducts() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let cards = document.getElementsByClassName("card");
+
+  for (let i = 0; i < cards.length; i++) {
+    let title = cards[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+
+    if (title.includes(input)) {
+      cards[i].style.display = "";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+}
